14f31ca61ca37538ecb8c14c4a43c80ec5b9908c
glf30 <greyson.frazier@gmail.com>
2025-06-12 09:18:49 -0400

    Initial commit for 2c-express-crud/

---
+++ assignment/data/index.js
@@ -0,0 +1,46 @@
+const ingredients = {
+  bread: ["white", "wheat", "sourdough", "rye", "multigrain"],
+
+  meat: ["turkey", "ham", "roast beef", "chicken", "bacon", "salami"],
+
+  cheese: [
+    "cheddar",
+    "swiss",
+    "provolone",
+    "mozzarella",
+    "pepper jack",
+    "Muenster",
+  ],
+
+  veggies: [
+    "lettuce",
+    "tomato",
+    "onion",
+    "cucumber",
+    "bell pepper",
+    "spinach",
+    "avocado",
+  ],
+
+  condiments: [
+    "mayonnaise",
+    "mustard",
+    "ketchup",
+    "relish",
+    "hot sauce",
+    "ranch dressing",
+  ],
+};
+
+const moreIngredients = {
+  fancy_meat: [
+    "honey glazed turkey",
+    "maple ham",
+    "roast beef",
+    "buffalo chicken",
+    "smoked bacon",
+    "italian salami",
+  ],
+};
+
+module.exports = { ingredients, moreIngredients };
